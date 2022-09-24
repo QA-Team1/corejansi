23abdd4711c013dc6b0f925910cc3d047f958975
shree <shree@10.0.0.88>
2022-09-24 14:42:24 -0400

    my first commit

---
+++ corej/src/corej/Testjava.java
@@ -0,0 +1,19 @@
+package corej;
+
+public class Testjava {
+ 
+
+		public static void main(String[] args) 
+		{
+			// TODO Auto-generated method stub
+			int num = 10;
+			if (num % 2 == 0)
+			{
+				System.out.println("Enter num is even");
+			}
+			else
+				System.out.println("Enter num is odd");
+		}
+
+	}
+
